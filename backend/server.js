0557c16426dc596a4fb98ce20e3696f18f70a184
Regina <reginatam429@gmail.com>
2019-09-09 18:59:13 -0700

    make it better

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,15 @@ app.use('/user', userRouter);
 app.use('/events', eventsRouter);
 app.use('/savedevents', savedeventsRouter);
 
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
+  
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
