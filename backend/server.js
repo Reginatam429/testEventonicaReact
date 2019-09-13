6cfe5badb3ee64ea2aa1a7c6e8f1f12a3b1272fe
Regina <reginatam429@gmail.com>
2019-09-13 15:38:10 -0700

    added stuff on server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // const bodyParser = require('body-parser');
 const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
 
 require('dotenv').config();
 
@@ -36,7 +37,13 @@ if (process.env.NODE_ENV === 'production') {
     });
   }
 
-
+  const client = new MongoClient(uri, { useNewUrlParser: true });
+  client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+  });
+  
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
