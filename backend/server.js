49fb22106ebcd59ac31b5e9b9d21185e6ec300cf
Regina <reginatam429@gmail.com>
2019-09-13 17:13:21 -0700

    updated

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,9 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 const mongoose = require('mongoose');
+app.set('port', process.env.PORT || 5000);
 
 app.use(cors());
 app.use(express.json());
@@ -31,12 +32,9 @@ if (process.env.NODE_ENV === 'production') {
     // Serve any static files
     app.use(express.static(path.join(__dirname, 'build')));
   // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    });
+   // app.get('*', function(req, res) {
+      //res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    //});
   }
 
-
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-});
+  app.listen(app.get('port'), () => console.log(`Server started on port ${app.get('port')}`));
